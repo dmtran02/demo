984bd3e832c61c5e2d4638f45852f4299bbf74a0
Duc <dmtran02@gmail.com>
2025-03-18 16:29:08 -0400

    built bare bones of rest api and start db

--- src/main/java/dev/chaepanyaprogramming/demo/run/RunRepository.java
+++ src/main/java/dev/chaepanyaprogramming/demo/run/RunRepository.java
@@ -16,6 +16,28 @@ public class RunRepository {
         return runs;
     }
 
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run) {
+        Optional<Run> existingRun = findById(run.id());
+        if (existingRun.isPresent()) {
+            // Update the existing run with the new values
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id() == id);
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+
     @PostConstruct
     // This annotation is used to mark a method that needs to be executed after dependency injection is done to perform any initialization.
     private void init() {
@@ -30,6 +52,12 @@ public class RunRepository {
                 LocalDateTime.now(),
                 LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
                 3,
+                Location.OUTDOOR));
+        runs.add(new Run(3,
+                "Third Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(120, ChronoUnit.MINUTES),
+                12,
                 Location.OUTDOOR));
     }
 }
